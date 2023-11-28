7d7a57410ba1a2b0bc93d56a8a4f0820e8bb46e5
Kerem Ciftci <krmciftci17@gmail.com>
2023-11-28 16:34:16 +0300

    I made the nav bar

---
+++ netflix-xlone/src/firebase.jsx
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBrIzwL-mlo_wJXm22I5kg4psQBKoW4UUw",
+    authDomain: "netflix-clone-kerem.firebaseapp.com",
+    projectId: "netflix-clone-kerem",
+    storageBucket: "netflix-clone-kerem.appspot.com",
+    messagingSenderId: "240844825617",
+    appId: "1:240844825617:web:fe35843c2b6cc65eb8fdb0"
+  };
