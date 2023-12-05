6f13eecf0ec86f57c4535d7a868f69b035514748
Kerem Ciftci <krmciftci17@gmail.com>
2023-12-05 16:45:20 +0300

    I added row component and implemeted TMDB API

--- netflix-xlone/src/App.jsx
+++ netflix-xlone/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Banner from './Banner'
 import HomeScreen from './HomeScreen'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   <div className="app">
 
     <HomeScreen/>
+    
   </div>
   )
 }
